18c3140ac887e58719a12247c77deb82cf97a139
yugandhar-2003 <126131125+yugandhar-2003@users.noreply.github.com>
2024-07-26 18:24:08 +0530

    Update r.js

--- videos/r.js
+++ videos/r.js
@@ -1,1 +1,40 @@
+let aboutButtonR = document.getElementById("aboutButtonR");
+let timeToVisitButtonR = document.getElementById("timeToVisitButtonR");
+let attractionsButtonR = document.getElementById("attractionsButtonR");
+
+let aboutTabR = document.getElementById("aboutTabR");
+let timeToVisitTabR = document.getElementById("timeToVisitTabR");
+let attractionsTabR = document.getElementById("attractionsTabR");
+
+timeToVisitTabR.style.display = "none";
+attractionsTabR.style.display = "none";
+
+aboutButtonR.onclick = function() {
+    aboutButtonR.style.backgroundColor = "white";
+    timeToVisitButtonR.style.backgroundColor = "#dae2ec";
+    attractionsButtonR.style.backgroundColor = "#dae2ec";
+    aboutTabR.style.display = "block";
+    aboutTabR.classList.add = "tabs-container";
+    timeToVisitTabR.style.display = "none";
+    attractionsTabR.style.display = "none";
+}
+
+timeToVisitButtonR.onclick = function() {
+    aboutButtonR.style.backgroundColor = "#dae2ec";
+    timeToVisitButtonR.style.backgroundColor = "white";
+    attractionsButtonR.style.backgroundColor = "#dae2ec";
+    aboutTabR.style.display = "none";
+    timeToVisitTabR.style.display = "block";
+    timeToVisitTabR.classList.add = "tabs-container";
+    attractionsTabR.style.display = "none";
+}
+attractionsButtonR.onclick = function() {
+    aboutButtonR.style.backgroundColor = "#dae2ec";
+    timeToVisitButtonR.style.backgroundColor = "#dae2ec";
+    attractionsButtonR.style.backgroundColor = "white";
+    aboutTabR.style.display = "none";
+    timeToVisitTabR.style.display = "none";
+    attractionsTabR.style.display = "block";
+    attractionsTabR.classList.add = "tabs-container";
+}
 
